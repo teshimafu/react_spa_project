e985d7293d1ff0316fa2a151c190c61ba1be16a2
teshimafu <teshima.fu@gmail.com>
2019-02-22 22:17:48 +0900

    create bootstrap navBar sample (#10)
    * create firebase.ts
    
    * add bootstrap

---
+++ client/src/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+import 'firebase/auth'
+
+const config = {
+    apiKey: "AIzaSyDCe-yB2Z6h8Fr_Hc_f_9FGzIgCwuKtGMY",
+    authDomain: "homepage-232202.firebaseapp.com",
+    databaseURL: "https://homepage-232202.firebaseio.com",
+    messagingSenderId: "10713409910",
+    projectId: "homepage-232202",
+    storageBucket: "homepage-232202.appspot.com"
+};
+
+initializeApp(config)
