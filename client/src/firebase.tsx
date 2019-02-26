8215a74e3da28bd4a1ba2118bb376ca7d026cc39
teshimafu <teshima.fu@gmail.com>
2019-02-27 00:12:08 +0900

    Develop (#14)
    * create firebase.ts
    
    * add bootstrap
    
    * add redux
    
    * fix firebase.json

---
+++ client/src/firebase.tsx
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+import 'firebase/auth'
+
+const config = {
+    apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+    authDomain: "homepage-232202.firebaseapp.com",
+    databaseURL: "https://homepage-232202.firebaseio.com",
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    projectId: "homepage-232202",
+    storageBucket: "homepage-232202.appspot.com"
+};
+
+initializeApp(config)
